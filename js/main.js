6de54830891c0e6e0c39aacc00ef9bede2e9508e
terehof <terehovsergej@gmail.com>
2015-05-28 22:14:42 +0300

    last

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   $('button.buy-now').click(function(){
+       $('.popup').show();
+       $('#TB_overlay').show();
+   });
+    $('p.close').click(function(){
+        $('.popup').hide();
+        $('#TB_overlay').hide();
+    });
+    $('#TB_overlay').click(function(){
+        $('.popup').hide();
+        $('#TB_overlay').hide();
+    });
+});
